1d7cd159eb45ff55348277daf2d9fc2d8c21a786
venevtseva_nina <kismnina00@gmail.com>
2022-03-28 17:14:05 +0300

    add section hero, section about-us, section gallery

---
+++ js/customChoices.js
@@ -0,0 +1,6 @@
+const element = document.querySelector('#selectChoices');
+const choices = new Choices(element, {
+  searchEnabled: false,
+  sorter: () => {},
+  position: 'bottom',
+});
--- js/swiper.js
+++ js/swiper.js
@@ -1,17 +1,30 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  // direction: 'horizontal',
-  // loop: true,
-  // autoplay: {
-  //   delay: 2000,
-  // },
+const swiperFirst = new Swiper('.swiper-first', {
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   },
+  autoplay: {
+    delay: 2000,
+  },
+});
 
+const swiperSecond = new Swiper('.swiper-second', {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  slidesPerGroup: 3,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
   a11y: {
     paginationBulletMessage: 'Перейти к слайду {{index}}',
+  },
+  zoom: {
+    maxRatio: 5,
   },
 
 });
