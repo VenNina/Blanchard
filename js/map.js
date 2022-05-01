508f274eb718cee230eca35853e11c139448628a
Nina <kismnina00@gmail.com>
2022-05-01 17:57:43 +0300

    add footer

---
+++ js/map.js
@@ -0,0 +1,21 @@
+ymaps.ready(init);
+
+function init() {
+  let myMap = new ymaps.Map("map", {
+    center: [55.758788495115844, 37.61637636268213],
+    zoom: 14,
+  });
+
+  let myPlacemark = new ymaps.Placemark([55.758788495115844, 37.61637636268213], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/marker-map.svg',
+    iconImageSize: [20, 20],
+  });
+
+  // myMap.geoObjects.add(myGeoObject);
+  myMap.geoObjects.add(myPlacemark);
+
+
+  // Выключим масштабирование колесом мыши
+  myMap.behaviors.disable('scrollZoom');
+}
