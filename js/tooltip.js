a7de72b74d9fe66bd9f3d3287edba525a0caef68
Nina <kismnina00@gmail.com>
2022-09-23 17:13:18 +0300

    add aria on dropdown

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,18 +1,18 @@
-tippy('#myButton1', {
+tippy('#my-tooltip-1', {
   content: 'Пример современных тенденций современная методология разработки',
   placement: 'top',
   arrow: true,
   theme: 'active',
 });
 
-tippy('#myButton2', {
+tippy('#my-tooltip-2', {
   content: 'Приятно, граждане, наблюдать, как сделанные на базе аналитики выводы вызывают у вас эмоции',
   placement: 'top',
   arrow: true,
   theme: 'active',
 });
 
-tippy('#myButton3', {
+tippy('#my-tooltip-3', {
   content: 'В стремлении повысить качество',
   placement: 'top',
   arrow: true,
